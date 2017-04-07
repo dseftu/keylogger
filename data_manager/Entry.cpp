328ca7629d4ea9bf62e98f68b901bc482e3e5d2e
Eric Smithson <ericsmithson@Erics-MacBook-Pro.local>
2017-04-07 19:32:22 -0400

    updated entry object

--- data_manager/Entry.cpp
+++ data_manager/Entry.cpp
@@ -3,39 +3,22 @@ using std::string
 
 class Entry {
     // all the values are held as ascii strings
-    string entryType, endTime, text, encoding, keyCombo, programName, activeWindow, startTime;
+    string startTime, endTime, duration, text, programName;
     public:
-        Entry(string);
         Entry(string, string, string, string, string, string);
         string toString();
 };
 
-Entry::Entry (string startTime) {
-    self.startTime = startTime;
-    self.entryType = "";
-    self.endTime = "";
-    self.text = "";
-    self.encoding = "";
-    self.keyCombo = "";
-    self.programName = "";
-    self.activeWindow = "";
-}
-
-Entry::Entry (string entryType, string startTime, string endTime, 
-                string text, string encoding, string keyCombo, 
-                string programName, string activeWindow) {
-    self.entryType = entryType;
+Entry::Entry (string startTime, string endTime, string duration, 
+                string text, string programName) {
     self.startTime = startTime;
     self.endTime = endTime;
+    self.duration = duration;
     self.text = text;
-    self.encoding = encoding;
-    self.keyCombo = keyCombo;
     self.programName = programName;
-    self.activeWindow = activeWindow;
 }
 
 Entry::toString() {
     string temp = "{\"active_process\": \"" + activeProcess + "\", \"process_id\": \"" + procid + "\", \"start_time\": \"" + startTime + "\", \"end_time\": \"" + endTime + "\", \"session_duration\": \"" + duration + "\", \"logged_keystrokes\": \"" + text + "\"}";
-    // string temp = "{\"entryType\" : \"" + entryType + "\", \"startTime\" : \"" + startTime + "\", \"endTime\" : \"" + endTime + "\", \"text\" : \"" + text + "\", \"encoding\" : \"" + encoding + "\", \"keyCombo\" : \"" + keyCombo + "\", \"program\" : \"" + programName + "\", \"activeWindow\" : \"" + activeWindow + "\"},";
     return temp;
 }
