37f78a12706825dcac8e2c380f549d811d7e5c1a
clubPotato <jhazelbaker@knights.ucf.edu>
2017-04-17 23:07:30 -0400

    Pushed Installer Files to GitHub

---
+++ DankLogger Installer/intall_test_code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	system("cmd.exe /c dir c:\\");
+
+	cout << "\nDankLogger test successful\n";
+
+	system("pause");
+
+	return 0;
+}
